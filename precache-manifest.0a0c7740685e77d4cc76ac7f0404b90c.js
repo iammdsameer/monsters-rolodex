662edaa22ccdae0c254b21907f91cf0ac59d69aa
Sameer <itsmdsameerkhan@gmail.com>
2020-05-28 17:48:34 +0545

    Updates

---
+++ precache-manifest.0a0c7740685e77d4cc76ac7f0404b90c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b9095675e0c1de3587630fbc32b0b0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "eda0096cb7fde8fc4f49",
+    "url": "/monsters-rolodex/static/css/main.ade472cc.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eda0096cb7fde8fc4f49",
+    "url": "/monsters-rolodex/static/js/main.bd8b4d67.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
